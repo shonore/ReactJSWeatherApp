2d1efb255735b7e9f88e3f1b460624add33b8985
Trashboat-99 <s.honore99@gmail.com>
2018-05-27 19:28:56 -0400

    Updated Weather component and added Titles.js file

--- src/App.js
+++ src/App.js
@@ -8,6 +8,15 @@ const API_KEY = "fcc9263fdb61327ad1d1a315c709d1fe";
 
 //initialize component. Creates an instance of the component
 class App extends React.Component {
+//In React 16 you can use state object. This object contains key/value pairs
+state = {
+  temperature: undefined,
+  city: undefined,
+  country: undefined,
+  humidity: undefined,
+  description: undefined,
+  error: undefined
+}
 //a custom method for getting the weather data. Using arrow function
 //the "e" argument prevents a full page refresh on submit
   getWeather = async (e) => {
@@ -17,12 +26,30 @@ class App extends React.Component {
     const country = e.target.elements.country.value;
     //using template strings as parms for fetch method.
     //template strings allow you to inject input values
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metrics`);
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=imperial`);
     //after the API call is made we need to convert the response to JSON format
     const data = await api_call.json();
-    console.log(data);
+    if(city && country){
+      console.log(data);
+      this.setState({
+        temperature: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        error: ""
+      });
+    } else {
+      this.setState({
+        temperature: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        description: undefined,
+        error: "Please enter a value for City and Country"
+      });
+    }
   }
-
   //display the data in the Component with render method
   render(){
     return (
@@ -31,7 +58,14 @@ class App extends React.Component {
       <div>
          <Titles />
          <Form getWeather={this.getWeather}/>
-         <Weather />
+         <Weather
+           temperature={this.state.temperature}
+           city={this.state.city}
+           country={this.state.country}
+           humidity={this.state.humidity}
+           description={this.state.description}
+           error={this.state.error}
+          />
       </div>
       //jsx can only return 1 parent element so everything you want to return has to be in single div
     );
---
+++ src/components/Titles.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Titles extends React.Component {
+  render () {
+    return (
+      <div>
+         <h1>Weather App</h1>
+         <p> This is a React JS app for returning information about the weather</p>
+      </div>
+    );
+  }
+};
+
+export default Titles;
